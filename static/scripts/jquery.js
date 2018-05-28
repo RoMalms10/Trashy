2980ed0b374cdb435c9b481a5292195506a6428b
romalms10 <romalms10@gmail.com>
2018-05-28 02:43:41 +0000

    Add function to embed map on to website

--- static/scripts/jquery.js
+++ static/scripts/jquery.js
@@ -1,2 +1,10 @@
-$(document).ready(function () {
-});
+// Initialize and add the map
+function initMap() {
+  // The location of San Francisco
+  var sf = {lat: 37.752, lng: -122.447};
+  // The map, centered at San Francisco
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 12, center: sf});
+  // The marker, positioned at San Francisco
+  var marker = new google.maps.Marker({position: sf, map: map});
+}
