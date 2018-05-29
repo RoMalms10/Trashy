378cdb56f32f95852d00388766ed31c984d9cc9d
romalms10 <romalms10@gmail.com>
2018-05-29 01:02:13 +0000

    Google Maps API working with CSV file

--- api.py
+++ api.py
@@ -3,6 +3,7 @@
 Flask App that handles API requests and redirects
 """
 from flask import Flask, render_template, url_for
+from flask import jsonify
 
 # Flask setup
 app = Flask(__name__)
@@ -23,6 +24,30 @@ def render_map_page():
     Serve the map webpage
     """
     return render_template('map.html')
+
+@app.route('/api/bins')
+def get_bins():
+    """
+    Gets all of the bin info from csv file
+    """
+    import csv
+
+    trash_list = []
+    with open('trash_cans.csv', newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',', quotechar="|")
+        for row in data:
+            new_dict = {}
+            new_dict['name'] = row[1]
+            lat = row[2].strip("\"")
+            lat = lat.strip("(")
+            lng = row[3].strip("\"")
+            lng = lng.strip(")")
+            lng = lng.strip(" ")
+            new_dict['location'] = {"lat": float(lat), "lng": float(lng)}
+            trash_list.append(new_dict)
+            
+
+    return jsonify(trash_list)
 
 if __name__ == "__main__":
     app.run(host=host, port=port)
--- static/scripts/jquery.js
+++ static/scripts/jquery.js
@@ -1,10 +1,38 @@
-// Initialize and add the map
-function initMap() {
-  // The location of San Francisco
-  var sf = {lat: 37.752, lng: -122.447};
-  // The map, centered at San Francisco
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 12, center: sf});
-  // The marker, positioned at San Francisco
-  var marker = new google.maps.Marker({position: sf, map: map});
-}
+(function() {
+  $(document).ready(function() {
+    $('.sidebar.big.icon').click(function() {
+      $('.ui.inverted.secondary.pointing.menu').toggleClass('ui inverted vertical menu')
+    });  
+  });
+
+  // Initialize and add the map
+  var initMap = function initializeGoogleMap() {
+
+    $.get("/api/bins", function(data) {
+
+      var sf = {lat: 37.752, lng: -122.447};
+      var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: sf});
+
+      if(data) {
+        for(var i = 0 ; i <= data.length-1; i++) {
+          var pin = new google.maps.Marker({position: new google.maps.LatLng(data[i].location.lat, data[i].location.lng), map: map, title: data[i].name });
+          var infowindow = new google.maps.InfoWindow({
+            content: "<div id=\"content\" style=\"color: black;\"><div id=\"bodyContent\">Location: "+ data[i].name+"</div></div>"
+          });
+//          pin.addListener('click', function() {
+//            infowindow.open(map, pin);
+          google.maps.event.addListener(pin, 'click', (function(pin, i) {
+            return function() {
+              infowindow.setContent(locations[i].lat);
+              infowindow.open(map, pin);
+            }
+          })(pin, i));
+        };
+  //      map.setCenter(data[0].location)    
+      };
+    })
+  }
+  window.initMap = initMap;
+})();
+
+// iffe
