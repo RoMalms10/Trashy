bb4cde46dde2ac7e89039c3f3f4f2a9a30a93820
romalms10 <romalms10@gmail.com>
2018-05-30 04:38:44 +0000

    Change to open source map

--- static/scripts/leaflet.js
+++ static/scripts/leaflet.js
@@ -7,11 +7,8 @@
   });
 
   var mymap = L.map('mapid').setView([37.752, -122.447], 16);
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1Ijoicm9tYWxtczEwIiwiYSI6ImNqaHF6c2NvbTA3dmkzMHBwaWwzZmhibWIifQ.RpiKEopngPIF8x2SnDR5-g'
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mymap);
 
   if (navigator.geolocation) {
