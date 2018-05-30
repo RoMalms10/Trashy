7588da72f9fe6cde11c1b92a4cbe75e33f82ef69
romalms10 <romalms10@gmail.com>
2018-05-30 03:15:56 +0000

    Create test file to see if database works

--- test_storage.py
+++ test_storage.py
@@ -3,9 +3,29 @@
 
 if __name__ == "__main__":
     import models
+    import csv
     marker = models.classes["Marker"]()
     print(marker.id)
     marker.save()
     print(models.storage.all())
+    marker.delete()
+    print(models.storage.all())
+    with open('trash_cans.csv', newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',', quotechar="|")
+        for row in data:
+            new_dict = {}
+            new_dict['name'] = row[1]
+            lat = row[2].strip("\"")
+            lat = lat.strip("(")
+            new_dict['latitude'] = float(lat)
+            lng = row[3].strip("\"")
+            lng = lng.strip(")")
+            lng = lng.strip(" ")
+            new_dict['longitude'] = float(lng)
+            break
+    print(new_dict)
+    marker = models.classes["Marker"](**new_dict)
+    marker.save()
+    print(marker)
     marker.delete()
     print(models.storage.all())
