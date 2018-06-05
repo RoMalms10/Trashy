514e5ed2445fdebdeb7cc73c43124944e53cc06a
romalms10 <romalms10@gmail.com>
2018-06-05 08:25:14 +0000

    change time_format

--- api.py
+++ api.py
@@ -82,7 +82,7 @@ def add_marker():
     most_recent = storage.get_user_submitted(current_user.id)
     print(most_recent)
     if len(most_recent) > 0:
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        time_format = "%Y-%m-%d %H:%M:%S.%f"
         recent = datetime.strptime(most_recent[0]["created_at"], time_format)
         # Get current time to check if a submit has been made recently
         present = datetime.utcnow()
