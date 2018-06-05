1fc600d66f37d39c74e47871e833522152209d0a
romalms10 <romalms10@gmail.com>
2018-06-05 17:51:18 +0000

    See if the user is authenticated

--- api.py
+++ api.py
@@ -66,7 +66,7 @@ def proximity_bins():
         prox_list = storage.proximity(post_info["latitude"], post_info["longitude"], radius*i)
         i = i + 1
     for dicts in prox_list:
-        if current_user is not None:
+        if current_user.is_authenticated():
             if dicts["user_id"] != current_user.id:
                 dicts.pop("user_id", None)
         else:
